6853ee74bed26cd90af5066ed09f12afcd8c7a97
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-03-26 09:38:19 -0700

    Add toggle feature to show hidden content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
-  // const aprospectivecoder = confirm("Interested what coding language matches your personality? Click ok for yes or cancel for no.")
-  //   if (aprospectivecoder) {
-  //     $("#interested").show();
-  //   } else {
-  //        $("#notinterested").show();
-  //   }
-  
+  const aprospectivecoder = confirm("Interested what coding language matches your personality? Click ok for yes or cancel for no.")
+    if (aprospectivecoder) {
+      $("#interested").show();
+    } else {
+         $("#notinterested").show();
+    }
+
+    $("#java").click(function() {
+      $(".java-hidden").toggle();
+      $(".java-showing").toggle();
+    })
+
+    $("#c").click(function() {
+      $(".c-hidden").toggle();
+      $(".c-showing").toggle();
+    })
+
+    $("#ror").click(function() {
+      $(".ruby-hidden").toggle();
+      $(".ruby-showing").toggle();
+    })
 })
