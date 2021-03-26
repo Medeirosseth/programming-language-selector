92b187ca96104e2847864d4a6a30684d4ec26ea9
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-03-26 11:25:06 -0700

    Add if/else syntax structure

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,44 @@ $(document).ready(function() {
          $("#notinterested").show();
     }
 
+    $("#formOne").submit(function(event){
+      event.preventDefault();
+      const entertainment = parseInt($("#entertainment").val());
+      const beverage = parseInt($("#beverage").val());
+      const app = parseInt($("#app").val());
+      const animal = parseInt($("#animal").val());
+      const computer = parseInt($("#computer").val());
+
+      if (entertainment == 1) {
+        if (beverage == 1) {
+          if (app == 1) {
+            if (animal == 1) {
+              if (computer == 1) {
+                $("#result").text("Java");
+              } else if (computer == 2) {
+                $("#result").text("C#");  
+              }
+            }
+          }
+        }
+      }
+      if (entertainment == 1) {
+        if (beverage == 1) {
+          if (app == 1) {
+            if (animal == 2) {
+              if (computer == 1) {
+                $("#result").text("Ruby");
+              } else if (computer == 2) {
+                $("#result").text("C#");  
+              }
+            }
+          }
+        }
+      }
+
+      $(".hide").show("#result");
+    })
+
     $("#java").click(function() {
       $(".java-hidden").toggle();
       $(".java-showing").toggle();
@@ -21,3 +59,4 @@ $(document).ready(function() {
       $(".ruby-showing").toggle();
     })
 })
+
