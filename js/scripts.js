238084c1fa9da6dc40b29a8d9a8dcb0576111927
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-03-26 09:07:20 -0700

    Add bulk of html and bootstrap containers

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // const aprospectivecoder = confirm("Interested what coding language matches your personality? Click ok for yes or cancel for no.")
+  //   if (aprospectivecoder) {
+  //     $("#interested").show();
+  //   } else {
+  //        $("#notinterested").show();
+  //   }
+  
+})
